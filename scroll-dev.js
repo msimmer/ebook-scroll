0f784f062a58a08943bf4c548db4e0eb7d90270c
Maxwell Simmer <maxwell.simmer@gmail.com>
2014-03-10 15:45:06 +0100

    book pages, json, page load

--- scroll-dev.js
+++ scroll-dev.js
@@ -1,19 +1,39 @@
 $(function() {
 
-    var url     = 'components/page-1.html',
-        main    = $('main');
+    var url = 'components/page-1.html',
+        main = $('main');
 
-    $.get({
-        url: url,
-        dataType: 'json',
-        success: function(data) {
+    function loadChapter(url) {
+        main.load(url);
+    }
+
+    function savePosition(){}
+    function getPosition(){}
+
+    var jsonUrl = 'data/bookData.json',
+        chapters = $('.chapters');
+
+    $.getJSON(jsonUrl, {
+        format: 'json'
+    })
+        .success(function(data) {
             console.log('success');
-            main.html(data);
-        },
-        error: function(xhr, textStatus, errorThrown) {
-            console.log('Error: ' + textStatus + ' ' + errorThrown);
-        }
-    });
-
+            $.each(data, function(i, o) {
+                $('<a/>', {
+                    text: o.title,
+                    href: o.src,
+                    click: function(e) {
+                        e.preventDefault();
+                        loadChapter(o.src)
+                    }
+                }).wrap('<li/>').appendTo('.chapters');
+            })
+        })
+        .fail(function() {
+            console.log(arguments);
+        })
+        .done(function() {
+            // console.log('done');
+        });
 
 });
