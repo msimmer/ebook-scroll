2cf5d953c29a40b4ca8e34329b9712ae6a94f9b9
Maxwell Simmer <maxwell.simmer@gmail.com>
2014-03-10 17:12:34 +0100

    localstorage, init, helpers

--- scroll-dev.js
+++ scroll-dev.js
@@ -1,38 +1,44 @@
 $(function() {
 
-    var readerData = readerData || {
-        currentPage:'' // (string), url
+    window.readerData = {
+        currentPage: '', // (string), url
+        firstPage: '', // (string), url
+        lastPage: '' // (string), url
     };
 
     // fns
-    var url = 'components/page-1.html',
+    var url = (readerData.currentPage !== '' ? readerData.currentPage : readerData.firstPage),
         main = $('main');
 
-    function updatedReaderData(prop, attr){
+    function updatedReaderData(prop, attr) {
         readerData[prop] = attr;
     }
 
     function loadChapter(url) {
         main.load(url);
-        updatedReaderData(currentPage, url);
+        updatedReaderData('currentPage', url);
+        setPosition();
     }
 
-    function savePosition() {
+    function setPosition() {
         var clientBook = {
-            'lastPage': readerData.currentPage
+            'currentPage': readerData.currentPage
         };
         localStorage.setItem('clientBook', JSON.stringify(clientBook));
     }
 
     function getPosition() {
-        var retrievedObject = localStorage.getItem('clientBook');
-        console.log('retrievedObject: ', JSON.parse(retrievedObject));
+        if (localStorage.getItem('clientBook') === null) {
+            setPosition();
+        }
+        var obj = localStorage.getItem('clientBook');
+        readerData.currentPage = JSON.parse(obj).currentPage;
     }
 
     // events
-    $(window).on('beforeunload', 'savePosition'));
+    $(window).on('beforeunload', 'setPosition');
 
-    // init
+    // get data, build dom
     var jsonUrl = 'data/bookData.json',
         chapters = $('.chapters');
 
@@ -40,8 +46,13 @@ $(function() {
         format: 'json'
     })
         .success(function(data) {
-            console.log('success');
             $.each(data, function(i, o) {
+                if (i === 0) {
+                    updatedReaderData('firstPage', o.src);
+                }
+                if (i === data.length - 1) {
+                    updatedReaderData('lastPage', o.src);
+                }
                 $('<a/>', {
                     text: o.title,
                     href: o.src,
@@ -49,14 +60,28 @@ $(function() {
                         e.preventDefault();
                         loadChapter(o.src);
                     }
-                }).wrap('<li/>').appendTo('.chapters');
+                }).appendTo($('<li/>').appendTo('.chapters'));
             })
         })
         .fail(function() {
             console.log(arguments);
         })
         .done(function() {
-            // console.log('done');
+            init();
         });
+
+
+    // init
+
+    function init() {
+
+        // check local storage for location
+        getPosition();
+
+        // if localstorage exists, it's already readerData.currentPage, if not it's readerData.firstPage
+        var page = (readerData.currentPage !== '' ? readerData.currentPage : readerData.firstPage);
+        loadChapter(page);
+    }
+
 
 });
