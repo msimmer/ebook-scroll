1bf32b2fd6b989653f487040370b8b73dd83161b
Maxwell Simmer <maxwell.simmer@gmail.com>
2014-03-12 11:24:12 +0100

    JSON promise then load, loads ch1 on first page visit

--- js/env.js
+++ js/env.js
@@ -55,6 +55,8 @@ App = {
         that.el.load(url);
         that.updatedReaderData('currentPage', url);
 
+        return that;
+
     },
 
     saveLocation: function() {
@@ -73,6 +75,9 @@ App = {
         };
 
         localStorage.setItem('clientBook', JSON.stringify(clientBook));
+
+        return that;
+
     },
 
     getUserPreferences: function() {
@@ -90,6 +95,9 @@ App = {
             };
             localStorage.setItem('userPreferences', JSON.stringify(userPreferences));
         }
+
+        return that;
+
     },
 
     updateUserPreferences: function() {
@@ -103,6 +111,9 @@ App = {
         };
 
         localStorage.setItem('userPreferences', JSON.stringify(userPreferences));
+
+        return that;
+
     },
 
     goToPreviousLocation: function() {
@@ -118,7 +129,9 @@ App = {
 
         setTimeout(function() {
             that.el.scrollTop(pos);
-        }, 50)
+        }, 50);
+
+        return that;
 
     },
 
@@ -135,71 +148,20 @@ App = {
                 currentPage: that.readerData.firstPage,
                 scrollPosition: {}
             };
+
+            that.readerData.currentPage = that.readerData.firstPage;
+            that.readerData.scrollPosition[that.readerData.firstPage] = 0;
+
             clientBook.scrollPosition[that.readerData.firstPage] = 0;
             localStorage.setItem('clientBook', JSON.stringify(clientBook));
         }
+
+        return that;
+
     },
 
     goToNextChapter: function() {
-        //
-    },
-
-    getJsonData: function() {
-
-        // get book data and insert into DOM
-        var that = this;
-
-        var jsonUrl = 'data/bookData.json',
-            chapters = $('.chapters');
-
-        var jsonData = $.getJSON(jsonUrl, {
-            format: 'json'
-        })
-            .success(function(data) {
-
-                $.each(data, function(i, o) {
-                    if (i === 0) {
-                        that.readerData.firstPage = o.src;
-                        // var page = (localStorage.getItem('clientBook') === null ? that.readerData.firstPage : that.readerData.currentPage);
-                        // that.readerData.currentPage = page;
-                    }
-                    if (i === data.length - 1) {
-                        that.readerData.lastPage = o.src;
-                    }
-                    $('<a/>', {
-                        text: o.title,
-                        href: o.src,
-                        click: function(e) {
-                            e.preventDefault();
-                            that.loadChapter(o.src);
-                            that.goToPreviousLocation();
-                        }
-                    }).appendTo($('<li/>').appendTo('.chapters'));
-                })
-            })
-            .fail(function() {
-                console.log(arguments);
-            });
-
-        jsonData.complete(function() {
-
-            // styles
-            that.removeElementStyles();
-            that.getUserPreferences();
-            that.setDomElements();
-            that.setStyles();
-            that.layout.adjustFramePosition();
-
-            that.getLocation();
-
-            // var page = (localStorage.getItem('clientBook') === null ? that.readerData.firstPage : that.readerData.currentPage);
-            // that.readerData.currentPage = page;
-            that.loadChapter(that.readerData.currentPage);
-
-            // readerData page location->DOM
-            that.goToPreviousLocation();
-        });
-
+        return this;
     }
 
 }
--- js/init.js
+++ js/init.js
@@ -2,8 +2,52 @@ App = App || {};
 $.extend(App, {
     init: function() {
 
-        this.bindEventHandlers();
-        this.getJsonData();
+        var that = this;
+
+        that.bindEventHandlers();
+
+        var jsonUrl = 'data/bookData.json',
+            chapters = $('.chapters');
+
+        var promiseJSON = $.getJSON(jsonUrl).success(function(data) {
+
+            $.each(data, function(i, o) {
+                if (i === 0) {
+                    that.readerData.firstPage = o.src;
+                }
+                if (i === data.length - 1) {
+                    that.readerData.lastPage = o.src;
+                }
+                $('<a/>', {
+                    text: o.title,
+                    href: o.src,
+                    click: function(e) {
+                        e.preventDefault();
+                        that.loadChapter(o.src);
+                        that.goToPreviousLocation();
+                    }
+                }).appendTo($('<li/>').appendTo('.chapters'));
+            })
+        });
+
+        $.when(promiseJSON).then(function(){
+
+            // get local storage or set it if it's === null
+            that.getLocation();
+            that.getUserPreferences();
+
+            // build DOM
+            that.removeElementStyles();
+            that.setDomElements();
+            that.setStyles();
+            that.layout.adjustFramePosition();
+
+            // load the last page read, or the first page if local storage wasn't set
+            that.loadChapter(that.readerData.currentPage);
+
+            // if local storage already existed, return to last reading position
+            that.goToPreviousLocation();
+        });
 
     }
 });
