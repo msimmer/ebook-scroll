e85faa3c17ffca97c09b3e31c7be4bbed3d519b9
Maxwell Simmer <maxwell.simmer@gmail.com>
2014-03-31 11:12:56 +0200

    isMobile improvement

--- app/scripts/env.js
+++ app/scripts/env.js
@@ -4,7 +4,14 @@ define(function() {
     var Env = {
 
         isMobile: function() {
-            return navigator.userAgent.match(/(iPad|iPhone|iPod|Android|webOS|BlackBerry|IEMobile|Opera Mini)/i) ? true : false;
+
+            if (/iPhone|iPod|acs|alav|alca|amoi|audi|aste|avan|benq|bird|blac|blaz|brew|cell|cldc|cmd-|dang|doco|eric|hipt|inno|ipaq|java|jigs|kddi|keji|leno|lg-c|lg-d|lg-g|lge-|maui|maxo|midp|mits|mmef|mobi|mot-|moto|mwbp|nec-|newt|noki|opwv|palm|pana|pant|pdxg|phil|play|pluc|port|prox|qtek|qwap|sage|sams|sany|sch-|sec-|send|seri|sgh-|shar|sie-|siem|smal|smar|sony|sph-|symb|t-mo|teli|tim-|tosh|tsm-|upg1|upsi|vk-v|voda|w3cs|wap-|wapa|wapi|wapp|wapr|webc|winw|winw|xda|xda-|up.browser|up.link|windowssce|iemobile|mini|mmp|symbian|midp|wap|phone|pocket|mobile|pda|psp/i.test(navigator.userAgent)
+            && !(/macintosh/i.test(navigator.userAgent))) {
+                return true;
+            } else{
+                return false;
+            }
+
         }
 
     };
