2c0b7c6e62e5cc73519c7baf3c8c7c2c66a7b46a
Maxwell Simmer <maxwell.simmer@gmail.com>
2014-06-07 17:03:51 +0200

    globals out of main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,25 +20,9 @@ require([
     'app',
     'shims/storage'
 ], function ($, App) {
-    // 'use strict';
+    'use strict';
 
     $(function () {
-
-        // window.ebookAppData = {
-        //     uuid: 'fiktion.' + localStorage.getItem('fiktion.referrer')
-        // };
-
-        // window.ebookAppData = {
-        //     uuid: 'fiktion.' + '21a7619e-ec78-4694-97c9-88a173795996' // test book
-        // };
-
-        window.ebookAppData = {
-            uuid: 'fiktion.' + 'fdbeeb28-6e83-4d3d-b96b-3bbd8c2d49c4' // about the reader
-        };
-
-        // window.ebookAppData = {
-        //     uuid: 'fiktion.' + 'c343c5e3-728f-44c5-915d-5146abc97097' // alff
-        // };
 
         var app = new App({
 
