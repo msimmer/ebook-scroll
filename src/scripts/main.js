6dd73ca5d388e5141b40c033a0df4c18bb7dd12f
Maxwell Simmer <maxwell.simmer@gmail.com>
2015-10-05 17:31:59 +0200

    app settings extend global options

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,16 @@ require.config({
 
 require(['modules/app'], function(App) {
 
-  var app = new App({
+  var options = {
     dev: false,
     jsonPath: window.location.href.match(/^http:\/\/local/) !== null ? 'http://localhost:8080/data/bookData.json' : '/wp-content/themes/Fiktion/data/bookData.json',
     debug: false,
     clearStorage: false,
     scrollSpeed: 10
-  });
+  };
+  var globalOptions = window.ebookAppData || {};
+  var settings = $.extend({}, options, globalOptions)
+  var app = new App(settings);
 
   $('html').removeClass('no-js').addClass('cursor js');
 
