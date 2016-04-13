1b605cfc5d439d5173a7a7d17f7478cee5cc840b
Maxwell Simmer <maxwell.simmer@gmail.com>
2016-04-13 16:13:43 +0200

    Update dev dependencies; Use express for routing

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,30 @@ var autoprefixer = require('gulp-autoprefixer');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-var livereload = require('gulp-livereload');
 var requirejs = require('gulp-requirejs');
 var del = require('del');
 var plumber = require('gulp-plumber');
-var connect = require('gulp-connect');
 var inject = require('gulp-inject');
+var express = require('express');
+var path = require('path');
 
 var production = false;
 var rev = '4322051a-c267-463c-9541-bc2ba1d3334a';
-var dirName = function() {
-  return production === true ? 'dist' : 'src';
-};
 
-gulp.task('styles', function() {
+var serverport = 3000;
+var server = express();
+// Map asset paths to `assets/`, e.g.,
+// ../bower_components/requirejs/require.js -> /assets/requirejs/require.js
+//
+server.use(express.static(path.join(__dirname, 'src/')));
+server.use('/data', express.static(path.join(__dirname, 'data/')));
+server.use('/assets', express.static(path.join(__dirname, 'resources/')));
+server.use('/assets', express.static(path.join(__dirname, 'bower_components/')));
+server.use('/assets', express.static(path.join(__dirname, 'src/')));
+
+gulp.task('styles', function () {
   var sassStyle = production === true ? 'compressed' : 'expanded';
-  var dir = dirName();
-  return gulp.src('src/styles/scss/main.scss')
+  return gulp.src('src/stylesheets/scss/main.scss')
     .pipe(sass({
       outputStyle: sassStyle,
       sourcemap: false
@@ -30,43 +37,38 @@ gulp.task('styles', function() {
     .pipe(rename({
       suffix: '.' + rev
     }))
-    .pipe(gulp.dest(dir + '/styles'));
+    .pipe(gulp.dest('src/stylesheets'));
 });
 
-gulp.task('jslint', function() {
-  return gulp.src('src/scripts/**/*.js')
+gulp.task('jshint', function () {
+  return gulp.src('src/javascripts/**/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('clean', function() {
-  del('dist/')
+gulp.task('clean', function () {
+  del(['dist/', '.tmp/']);
 });
 
-gulp.task('watch', function() {
-  var dir = dirName();
-  gulp.watch(dir + '/styles/**/*.scss', ['styles']);
+gulp.task('watch', function () {
   gulp.watch([
-    dir + '/scripts/**/*.js',
-    '!' + dir + '/scripts/main.build.' + rev + '.js'
+    'src/javascripts/**/*.js',
+    '!src/javascripts/main.build.' + rev + '.js'
   ], ['scripts']);
-  gulp.watch(dir + '/images/**/*', ['images']);
-
-  livereload.listen();
-  gulp.watch(dir + '/**').on('change', livereload.changed);
+  gulp.watch('src/stylesheets/**/*.scss', ['styles']);
 });
 
-gulp.task('copy', ['scripts', 'styles'], function() {
+gulp.task('copy', ['scripts', 'styles'], function () {
   // main.build.js is copied by `uglify` task
   // main.build.css is copied by `styles` task
   //
   var files = [
-    'src/scripts/require.js',
+    'src/javascripts/require.js',
     'src/data/**/*.*',
     'src/images/**/*.*',
     'src/components/**/*.*',
-    'src/styles/fonts/**/*.*',
+    'src/stylesheets/fonts/**/*.*',
     'src/*.png',
     'src/*.html'
   ];
@@ -76,18 +78,16 @@ gulp.task('copy', ['scripts', 'styles'], function() {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('envProduction', function() {
-  return production = true;
+gulp.task('envProduction', function () {
+  production = true;
+  return production;
 });
 
-gulp.task('inject', function() {
+gulp.task('inject', function () {
   gulp.src('./src/index.html')
     .pipe(inject(gulp.src([
-      'src/bower_components/jquery/jquery.min.js',
-      'src/bower_components/requirejs/require.js',
-      'src/scripts/main.build.' + rev + '.js',
-      'src/styles/main.css',
-      'src/styles/**/*.css'
+      'src/stylesheets/main.css',
+      'src/stylesheets/**/*.css'
     ], {
       read: false
     }), {
@@ -96,59 +96,53 @@ gulp.task('inject', function() {
         'src'
       ],
       relative: false,
-      transform: function(filepath, file, i, length) {
-        var relpath = production === true ? '/wp-content/themes/Fiktion' : '';
-        var type = null !== filepath.match(/\.js$/) ? 'js' : 'css';
-        var el;
-        if (type === 'css') {
-          el = '<link rel="stylesheet" type="text/css" href="' + relpath + filepath + '">';
-        } else if (type === 'js') {
-          el = '<script type="text/javascript" src="' + relpath + filepath + '"></script>';
-        }
-        return el;
+      transform: function (filepath, file, i, length) {
+        return '<link rel="stylesheet" type="text/css" href="/assets' + filepath + '">';
       }
     }))
     .pipe(gulp.dest('src'));
 });
 
-gulp.task('server', function() {
-  var dir = dirName();
-  connect.server({
-    root: dir,
-    livereload: true
-  });
+gulp.task('connect', function () {
+  server.listen(serverport);
+  gutil.log(gutil.colors.green('Listening on http://localhost:' + serverport));
 });
 
-gulp.task('scripts', function() {
-  return gulp.src('./src/scripts/main.build.js')
-    .pipe(plumber(function(error) {
+gulp.task('scripts', function () {
+  return gulp.src('./src/javascripts/main.build.js')
+    .pipe(plumber(function (error) {
       gutil.log(gutil.colors.red('Error: ' + error.message));
       this.emit('end');
     }))
     .pipe(requirejs({
       name: 'main',
-      baseUrl: './src/scripts',
+      baseUrl: './src/javascripts',
       out: './main.build.js'
     }))
     .pipe(rename({
       suffix: '.' + rev
     }))
-    .pipe(gulp.dest('./src/scripts'));
+    .pipe(gulp.dest('./src/javascripts'));
 });
 
-gulp.task('uglify', ['copy'], function() {
-  return gulp.src('src/scripts/main.build.' + rev + '.js')
+gulp.task('uglify', ['copy'], function () {
+  return gulp.src('src/javascripts/main.build.' + rev + '.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist/scripts'));
+    .pipe(gulp.dest('dist/javascripts'));
+});
+
+gulp.task('depends', function () {
+  // madge --circular ./src/javascripts/
+  // madge --format amd --image map.png --require-config ./src/javascripts/config.js ./src/javascripts/
 });
 
 gulp.task('serve', [
   'scripts',
   'styles',
   'inject',
-  'server',
+  'connect',
   'watch'
-], function() {});
+], function () {});
 
 gulp.task('build', [
   'envProduction',
@@ -158,4 +152,4 @@ gulp.task('build', [
   'inject',
   'copy',
   'uglify'
-], function() {});
+], function () {});
